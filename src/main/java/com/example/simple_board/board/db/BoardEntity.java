c18683ee006958cccf2ff084e2a303a5a66d059f
mkim4433 <mkim4433@gmail.com>
2026-03-06 15:22:54 +0900

    Initial commit

---
+++ src/main/java/com/example/simple_board/board/db/BoardEntity.java
@@ -0,0 +1,27 @@
+package com.example.simple_board.board.db;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "board")
+public class BoardEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String boardName;
+
+    private String status;
+}
+
+
